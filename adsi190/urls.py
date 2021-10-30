17cd3e8db9025c53e284fe3a5c9cbe2a70027c20
John W Martinez <jwmartinez88@gmail.com>
2021-10-30 17:53:00 -0500

    ClÃase 3 - Ajustes de redireccion, proceso de login, post, etc

--- adsi190/urls.py
+++ adsi190/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.saludo),
+    path('login/', views.login),
+    path('', views.saludo, name='home'),
     path('admin/', admin.site.urls),
 ]
--- adsi190/views.py
+++ adsi190/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.contrib.auth import login as lg
+from django.contrib.auth import authenticate
+from django.shortcuts import redirect
+
 def saludo(request):
     return render(request, 'index.html', {
         'mensaje': 'Bienvenidos a nuestro proyecto',
@@ -15,7 +19,7 @@ def saludo(request):
             {
                 'nombre': 'John',
                 'edad': '33',
-                'aprobo': False,
+                'aprobo': True,
             },
             {
                 'nombre': 'Milo',
@@ -24,5 +28,41 @@ def saludo(request):
             },
         ],
         'saludo': 'Buenas!',
+        'slider': [
+            {
+                'src':'img/fotografo-paisajes.jpeg',
+                'class':'d-block w-100',
+                'alt':'Descripción de foto 1',
+                'activo':True,
+            },
+            {
+                'src':'img/lago-paisaje.jpeg',
+                'class':'d-block w-100',
+                'alt':'Descripción de foto 2',
+                'activo':False,
+            },
+            {
+                'src':'img/luna-atardecer.jpg',
+                'class':'d-block w-100',
+                'alt':'Descripción de foto 3',
+                'activo':False,
+            },
+        ]
     })
     return HttpResponse("Hola Johnsito")
+
+
+def login(request):
+
+    if request.method == 'POST':
+        usuario = request.POST.get('usuario')
+        password = request.POST.get('password')
+        usuario = authenticate(username=usuario, password=password)
+        if usuario:
+            lg(request, usuario)
+            print("el usuario se ha logueado")
+            return redirect('home')
+        else:
+            print("El usuario no existe")
+
+    return render(request, 'users/login.html', {})
